6b49cefe010ddc8eb9af6d84518130a90ebe25ca
dewtx29 <dewtx29@gmail.com>
2016-09-19 21:46:46 +0700

    coding bestEvolution override

---
+++ a_dewdd_skyblock_lv_1000_generator/src/a_dewdd_skyblock_lv_1000_generator/EventListenerOverride.java
@@ -0,0 +1,12 @@
+package a_dewdd_skyblock_lv_1000_generator;
+
+import ga_optimization_api.Chromosome;
+
+public class EventListenerOverride extends ga_optimization_api.EventListener {
+
+	@Override
+	public void theBestOfEachEvolution(Chromosome cho) {
+		
+	}
+
+}
