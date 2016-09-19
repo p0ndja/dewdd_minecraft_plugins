b28ef431f4c3d24071aeae558c64e16284abd113
dewtx29 <dewtx29@gmail.com>
2016-09-19 21:47:37 +0700

    change bedrock to barrier (stop Damage Entity

--- dewdd_stop_damageentity/src/stopdamageentity/DigEventListener2.java
+++ dewdd_stop_damageentity/src/stopdamageentity/DigEventListener2.java
@@ -11,12 +11,42 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DigEventListener2 implements Listener {
 	
 	public JavaPlugin ac = null;
+	
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void eventja(EntityDamageByEntityEvent e) {
+		Entity ee = e.getEntity();
+		if (ee == null) {
+			e.setCancelled(true);
+
+			return;
+		} else {
+			if (ee instanceof LivingEntity) {
+				int search = 5;
+
+				boolean found = false;
+				for (int i = -search; i <= search; i++) {
+					if (ee.getLocation().getBlock().getRelative(0	, i, 0).getType() == Material.BARRIER) {
+						found = true;
+						break;
+					}
+				}
+
+				if (found == true) {
+					e.setCancelled(true);
+
+					return;
+				}
+			}
+		}
+
+	}
 
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void eventja(EntityDamageEvent e) {
